80c800c0d5270e685411bbfec863e5b7f7aada4f
kgc00Zenbook <kirbygc00@gmail.com>
2020-05-03 20:44:09 -0700

    finalized testing support.  may break assembly def down to finer grain size in future.

--- Assets/Scripts/Tests/Editor/Sandbox.cs
+++ Assets/Scripts/Tests/Editor/Sandbox.cs
@@ -1,6 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-// using Abilities.Modifiers;
+﻿using System.Collections.Generic;
+using Abilities.Modifiers;
 using NUnit.Framework;
 using UnityEngine;
 
@@ -11,13 +10,21 @@ namespace Tests
         [Test]
         public void Test()
         {
-            // var modifier = new MarkOnHitModifier(null);
-            // List<AbilityModifier> Modifiers = new List<AbilityModifier>();
-            // Modifiers.Add(modifier);
-            // Modifiers.Add(new DoubleDamageModifier(null));
-            //
-            // Debug.Log("new");
-            // Assert.Contains(modifier, Modifiers);
+            var modifier = new MarkOnHitModifier(null);
+            var ddMod = new DoubleDamageModifier(null);
+            List<AbilityModifier> Modifiers = new List<AbilityModifier>();
+            Modifiers.Add(modifier);
+            Modifiers.Add(ddMod);
+
+            Debug.Log($"Count: {Modifiers.Count}");
+
+            Assert.Contains(modifier, Modifiers);
+            Assert.AreSame(modifier, Modifiers[0]);
+
+            Modifiers.Remove(modifier);
+
+            Assert.AreSame(ddMod, Modifiers[0]);
+            Assert.AreEqual(Modifiers.Count, 1);
         }
     }
 }

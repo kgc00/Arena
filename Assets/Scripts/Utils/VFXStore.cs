c60df44799fd77fa676b0e2e3eabb89e2ff82079
kgc00 <kirbygc00@gmail.com>
2021-05-16 21:19:46 -0700

    concealedddd

---
+++ Assets/Scripts/Utils/VFXStore.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace Utils {
+    [CreateAssetMenu(fileName = "FILENAME", menuName = "MENUNAME", order = 0)]
+    public class VFXStore : ScriptableObject {
+        
+    }
+}
